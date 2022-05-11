6b57a35c5a1bc4958cf27a8354079d535579941c
amannem <amannem@gap.com>
2022-05-11 20:53:55 +0530

    Introduce changes for card component

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function Table() {
+
+    const [approvals, setApprovals] = useState<number>(1);
+    const [declines, setDeclines] = useState<number>(0);
+
+    const [appNotes, setAppNotes] = useState<string>("");
+    const [decNotes, setDecNotes] = useState<string>("");
+
+    const handleApprovals = () => {
+        setApprovals(approvals => approvals + 1);
+    }
+
+    const handleDeclines = () => {
+        setDeclines(declines => declines + 1);
+    }
+
+    const handleAppNoteUpdates = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        setAppNotes(evt.target.value);
+    }
+
+    const handleDecNoteUpdates = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        setDecNotes(evt.target.value);
+    }
+
     return (
         <table>
             <thead>
@@ -12,9 +35,18 @@ function Table() {
             </thead>
             <tbody>
                 <tr>
-                    <td>1</td>
-                    <td>0</td>
-                </tr></tbody>
+                    <td>{approvals}</td>
+                    <td>{declines}</td>
+                </tr>
+                <tr>
+                    <td><input className='embeddedbtn' type='text' value={appNotes} onChange={handleAppNoteUpdates}/></td>
+                    <td><input className='embeddedbtn' type='text' value={decNotes} onChange={handleDecNoteUpdates}/></td>
+                </tr>
+                <tr>
+                    <td><input className='embeddedbtn' type='button' value='Approve' onClick={handleApprovals}/></td>
+                    <td><input className='embeddedbtn' type='button' value='Decline' onClick={handleDeclines}/></td>
+                </tr>
+            </tbody>
         </table>
     )
 }

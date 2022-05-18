db1635ac0b3628fe1c9122c7d89cf959e7a30793
amannem <amannem@gap.com>
2022-05-18 21:26:39 +0530

    Node background color

--- src/nodesGenerator.tsx
+++ src/nodesGenerator.tsx
@@ -5,7 +5,11 @@ let nodes:Node[] = [];
 for (let i = 0; i < 100; i++){
     let xPosition = (i%9) * 100;
     let yPosition = (i/9) * 100;
-    nodes.push({ id: `${i+1}`, data: { label: `Node ${i + 1}` }, position: { x: xPosition, y: yPosition }} as Node)
+    let backgroundColor = (i%2 === 0) ? '#4169e1' : '#00FF00';
+    nodes.push({ id: `${i+1}`, 
+                data: { label: `Node ${i + 1}` }, 
+                position: { x: xPosition, y: yPosition },
+                style: {backgroundColor: backgroundColor} } as Node)
 }
 
 export default nodes;
